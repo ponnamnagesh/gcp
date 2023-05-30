8e282e69ac04168ddc3459943e9fd69abe66c2a6
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2023-05-29 20:26:09 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,1 +1,4 @@
   filename = "new_${var.env == "prod" ? local_file.example.filename : var.env == "dev" ? "dev.txt" : var.env == "sbx" ? "sbx.txt" : "exercise.txt"}"
+
+
+  filename = var.env == "prod" ? local_file.example.filename : var.env == "dev" ? "dev.txt" : var.env == "sbx" ? "sbx.txt" : "exercise.txt"
