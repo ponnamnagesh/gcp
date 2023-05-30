8de9b2d44d0b249bd067a25bd13b134fd59cef66
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2023-05-29 21:47:19 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -5,3 +5,6 @@
 
   filename = var.env == "prod" ? "new_excercise.txt" : var.env == "dev" ? "dev.txt" : var.env == "sbx" ? "sbx.txt" : "exercise.txt"
 
+  value = substr(module.project_ref.data.alerting.notification_channel, index(module.project_ref.data.alerting.notification_channel, "/") + 1)
+
+
