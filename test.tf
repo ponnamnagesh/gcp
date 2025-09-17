6498f136f5efda4fe8ba1600acd14da40e4cf962
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2025-09-17 14:30:37 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,20 +1,50 @@
-cd-workflow-prod:
-  name: CD - Workflow for Prod Deployments
-  needs: validate-cd-user
-  if: github.event_name == 'workflow_dispatch'
-  environment: ${{ inputs.envname }}
-  strategy:
-    fail-fast: false
-    max-parallel: 1
-    matrix:
-      server: ${{ fromJSON(vars.ENV_SERVER_NAME) }}
-  uses: charlesschwab/samda-action-workflows/.github/workflows/cd-python-scp-deploy.yml@main
-  with:
-    SERVER_NAME: ${{ matrix.server }}
-    PATH_TO_FILES: ${{ vars.PATH_TO_FILES }}
-    DESTINATION_PATH: ${{ vars.DESTINATION_PATH }}
-    RELEASE_VERSION: ${{ inputs.RELEASE_VERSION }}
-    ENV_NAME: ${{ inputs.envname }}
-    SAFEGUARD_URL: ${{ vars.SAFEGUARD_URL }}
-    SERVICE_ACNT: ${{ vars.SERVICE_ACNT }}
-  secrets: inherit
+name: CD workflow for deployments
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.ref }}
+  cancel-in-progress: true
+
+on:
+  workflow_dispatch:
+    inputs:
+      RELEASE_VERSION:
+        description: 'Release Version (ex: 1.0.1)'
+        type: string
+        required: true
+      envname:
+        description: 'Environment to deploy to'
+        type: choice
+        required: true
+        options:
+          - sandbox
+          - dev
+          - qa
+          - uat
+          - prod
+
+jobs:
+  validate-cd-user:
+    name: CD - Validate User
+    uses: charlesschwab/samda-action-workflows/.github/workflows/cd-common-validate-user.yml@main
+    secrets: inherit
+
+  cd-workflow-deploy:
+    name: CD - Workflow for Prod Deployments
+    needs: validate-cd-user
+    if: github.event_name == 'workflow_dispatch'
+    environment: ${{ inputs.envname }}                # pulls vars/secrets from selected Environment
+    strategy:
+      fail-fast: false
+      max-parallel: 1
+      matrix:
+        server: ${{ fromJSON(vars.ENV_SERVER_NAME) }} # e.g. ["SVM4090BDV","SVM4091BDV"]
+    uses: charlesschwab/samda-action-workflows/.github/workflows/cd-python-scp-deploy.yml@main
+    with:
+      SERVER_NAME: ${{ matrix.server }}
+      PATH_TO_FILES: ${{ vars.PATH_TO_FILES }}
+      DESTINATION_PATH: ${{ vars.DESTINATION_PATH }}
+      RELEASE_VERSION: ${{ inputs.RELEASE_VERSION }}
+      ENV_NAME: ${{ inputs.envname }}
+      SAFEGUARD_URL: ${{ vars.SAFEGUARD_URL }}
+      SERVICE_ACNT: ${{ vars.SERVICE_ACNT }}
+    secrets: inherit
